9f85f671e7373a03f4b4c40e8d7bb151d5d1cdcb
Veronica-Alfr <veagalves@gmail.com>
2022-02-11 19:57:34 -0300

    Função para remoção do hamburguer em andamento

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const button = document.querySelector('button')
+// const button = document.querySelector('button')
 
-button.addEventListener();
+// button.addEventListener();
+
+const menuHamburguer = () => {
+    const menu = document.querySelector("#menu");
+    menu.addEventListener('click', () => {
+        const imgMenu = document.querySelector("#img-hamburguer");
+        menu.removeChild(imgMenu);
+    });
+}
+
+window.onload = () => {
+  menuHamburguer();
+}
