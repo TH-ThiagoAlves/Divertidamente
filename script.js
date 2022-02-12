038aeedfcc8037aff01f482631ff488fbc19b32d
Veronica-Alfr <veagalves@gmail.com>
2022-02-12 17:55:09 -0300

    script.js modificado

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
 // const button = document.querySelector('button')
 
 // button.addEventListener();
-
-const menuHamburguer = () => {
-    const menu = document.querySelector("#menu");
-    menu.addEventListener('click', () => {
-        const imgMenu = document.querySelector("#img-hamburguer");
-        menu.removeChild(imgMenu);
-    });
-}
-
-window.onload = () => {
-  menuHamburguer();
-}
