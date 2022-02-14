038c661d7bb5f3ac63b477f4a4db9b9d0ae254d6
Yuut <ingride.reis.b@gmail.com>
2022-02-13 22:56:29 -0300

    TODOS OS ERROS FINALMENTE CONSERTADO COM A FORÇA DO ODIO

--- src/script.js
+++ src/script.js
@@ -34,16 +34,16 @@ const guardedFeelings = () => {
 
 const li = () => {
   emotes.forEach(element => {
-      element.addEventListener('click', sumEmot);
+    element.addEventListener('click', sumEmot);
   });
 }
 const il = () => {
-  emotes.forEach(element => {
-      element.removeEventListener('click', sumEmot)
-  });
+ emotes.forEach(element => {
+   element.removeEventListener('click', sumEmot);
+ });
 }
 
-const grafico = (emoticon) => {
+const grafico = () => {
   Highcharts.chart('container', {
     chart: {
       plotBackgroundColor: null,
@@ -68,69 +68,78 @@ const grafico = (emoticon) => {
       name: 'você sentiu',
       colorByPoint: true,
       data: [
-      {
-        name: 'tristeza',
-        y: emoticon[1],
-        sliced: true,
-        selected: true
-      }, 
-      {
-        name: 'medo',
-        y: emoticon[3]
-      }, 
-      {
-        name: 'alegria',
-        y: emoticon[0]
-      },
-      {
-        name: 'raiva',
-        y: emoticon[2]
-      }, 
-      {
-        name: 'nojo',
-        y: emoticon[4]
-      }]
+        {
+          name: 'tristeza',
+          y: emoticon[1],
+          sliced: true,
+          selected: true
+        },
+        {
+          name: 'medo',
+          y: emoticon[3]
+        },
+        {
+          name: 'alegria',
+          y: emoticon[0]
+        },
+        {
+          name: 'raiva',
+          y: emoticon[2]
+        },
+        {
+          name: 'nojo',
+          y: emoticon[4]
+        }]
     }]
   });
 }
 
 const sumEmot = (event) => {
   const armz = event.target.parentNode;
+  console.log(armz.id)
   switch (armz.id) {
     case 'alegria':
       emoticon[0] += 1;
-      grafico(emoticon);
+      grafico();
       return il();
     case 'tristeza':
       emoticon[1] += 1;
-      grafico(emoticon);
+      grafico();
       return il();
     case 'raiva':
       emoticon[2] += 1;
-      grafico(emoticon);
+      grafico();
       return il();
     case 'medo':
       emoticon[3] += 1;
-      grafico(emoticon);
+      grafico();
       return il();
     case 'nojo':
       emoticon[4] += 1;
-      grafico(emoticon);
+      grafico();
       return il();
   }
 };
 
-const temporizador = () => {
-  const dateAtual = new Date();
-  let dateSaved = parseInt(form[index].data[0] + form[index].data[1]);
-  if (dateSaved.getDate() !== dateAtual.getDate()) {
-    return li();
+ const temporizador = () => {
+  let dateAtual = '';
+  const index = JSON.parse(localStorage.getItem('feelings')).length -1;
+  dateSaved =  JSON.parse(localStorage.getItem('feelings'))[index].data;
+   const dia = new Date();
+  if (localStorage.getItem('feelings') !== null) {
+    dateAtual = `${dia.getDate()}/${dia.getMonth() + 1}/${dia.getFullYear()}`
   }
-}
+  if (dateSaved !== dateAtual) {
+    return li();
+    
+  }else{
+    return il();
+  }
+}; 
 
 const alterarHtml = (objet) => {
-    const recomenda = document.querySelector('#recomendações');
-    recomenda.innerHTML = `<ul>
+  const recomenda = document.querySelector('#recomendações');
+  recomenda.innerHTML = `<ul>
                            <li>${objet.meditação}<li>
                            <li>${objet.artigo}<li>
                            <li>${objet.video}<li>
@@ -155,8 +164,11 @@ const analise2 = () => {
       return alterarHtml(medo);
     case 4:
       return alterarHtml(nojo);
-    }
+  }
 }
+
+
+
 
 const analise = () => {
   const verifica = emoticon.filter((element) => element == number);
@@ -165,58 +177,59 @@ const analise = () => {
 };
 
 const alegria = {
-    meditação: 'a',
-    artigo: 'b',
-    video: 'c',
-    mensagem: 'd',
-    bonus: 'a',
+  meditação: 'a',
+  artigo: 'b',
+  video: 'c',
+  mensagem: 'd',
+  bonus: 'a',
 
 }
 
 const tristeza = {
-    meditação: 'a',
-    artigo: 'b',
-    video: 'c',
-    mensagem: 'd',
-    bonus: 'a',
+  meditação: 'a',
+  artigo: 'b',
+  video: 'c',
+  mensagem: 'd',
+  bonus: 'a',
 }
 
 const raiva = {
-    meditação: 'a',
-    artigo: 'b',
-    video: 'c',
-    mensagem: 'd',
-    bonus: 'a',
+  meditação: 'a',
+  artigo: 'b',
+  video: 'c',
+  mensagem: 'd',
+  bonus: 'a',
 }
 const medo = {
-    meditação: 'a',
-    artigo: 'b',
-    video: 's',
-    mensagem: 'd',
-    bonus: 'c',
+  meditação: 'a',
+  artigo: 'b',
+  video: 's',
+  mensagem: 'd',
+  bonus: 'c',
 }
 
 const nojo = {
-    meditação: 'a',
-    artigo: 'b',
-    video: 'c',
-    mensagem: 'd',
-    bonus: 'v',
+  meditação: 'a',
+  artigo: 'b',
+  video: 'c',
+  mensagem: 'd',
+  bonus: 'v',
 }
 
 const generico = {
-    meditação: 'a',
-    artigo: 'b',
-    video: 'c',
-    mensagem: 'd',
-    bonus: 'v',
+  meditação: 'a',
+  artigo: 'b',
+  video: 'c',
+  mensagem: 'd',
+  bonus: 'v',
 }
 
 window.onload = () => {
   li();
-  grafico();
+  grafico(emoticon);
   analise();
   button.addEventListener('click', guardedFeelings);
+  temporizador();
 }
 
 //module.exports = { guardedFeelings, li, sumEmot}
