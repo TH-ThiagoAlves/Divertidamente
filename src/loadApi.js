1e9dfff41feb04a41d980f9869e09033d2b50089
Maju Domenciano <juliadomenciano@hotmail.com>
2022-02-14 12:19:20 -0300

    tentativa de request da api

--- src/loadApi.js
+++ src/loadApi.js
@@ -2,10 +2,10 @@ const CLIENT_ID = '1010053455816-rga0m6g4civlte6a56sqdcpe25rp7ie1.apps.googleuse
 const CLIENT_SECRET = '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'
 const API_KEY = 'AIzaSyC7yfbRoRh50JMEK32yQK9Hx02nqodktU0'
 const emotion = 'happy'
+const maxResults = 5
 
-console.log('oi')
 
-const getApi = async (API_KEY, emotion, 5) => {
+const getApi = async (API_KEY, emotion, maxResults) => {
 /*   const obj = {
     method: 'GET',
     header: {
@@ -15,12 +15,15 @@ const getApi = async (API_KEY, emotion, 5) => {
     
   } */
 
-  const url = "https://www.googleapis.com/youtube/v3/search?key=" + API_KEY + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + emotion,
-  const response = await fetch(url)
+  const url = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}
+  &part=snippet&q=${emoticon}&maxResults=${maxResults}`;
+  console.log(url)
+  const response = await fetch(url, obj)
+  console.log(response)
   const data = await response.json();
   console.log(data);
 }
 
-window.onload = () => {
-  await getApi();
+window.onload = async () => {
+  const load = await getApi(API_KEY, emotion, listNumber);
 }
